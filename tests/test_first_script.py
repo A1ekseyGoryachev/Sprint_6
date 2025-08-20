11a602a3b3ac18b747c964211b2daade9ca9439f
Alexey Goryachev <kontolsky@yandex.ru>
2025-08-20 16:49:20 +0300

    Corrected files test_first_script.py, test_second_script.py, data.py, reimported  allure-results

--- tests/test_first_script.py
+++ tests/test_first_script.py
@@ -2,8 +2,7 @@ import allure
 
 
 from pages.page_one import PageOne
-from data import FirstScript
-from data import FirstCommentForCourier
+from data import *
 from pages.page_two import PageTwo
 from pages.transitions import Transitions
 from curl import *
@@ -27,12 +26,24 @@ class TestOrderAccordingFirstScript:
         first_script_page_two.press_order_button()
         first_script_page_two.press_yes_button()
         order_confirmation = first_script_page_two.get_text_confirmation()
-        assert order_confirmation == "Посмотреть статус"
+        assert order_confirmation == CHECKUP_STATUS
 
     @allure.title("Проверка перехода со страницы оформления заказа на главную страницу сайта")
     def test_transition_from_order_page_to_main_page(self, driver):
-        scooter_confirmation = TestOrderAccordingFirstScript()
-        scooter_confirmation.test_order_form_with_first_script(driver)
+        name = FirstScript.name
+        last_name = FirstScript.last_name
+        address = FirstScript.address
+        phone = FirstScript.phone
+        first_script_page_one = PageOne(driver)
+        first_script_page_one.press_main_page_upper_order_button()
+        first_script_page_one.press_cookies_button()
+        first_script_page_one.fill_up_order_form_page_one(name, last_name, address, phone)
+        first_script_page_one.press_next_button()
+        first_script_page_two = PageTwo(driver)
+        comment = FirstCommentForCourier.comment_for_first_script
+        first_script_page_two.fill_up_order_form_page_two_with_first_set_of_data(comment)
+        first_script_page_two.press_order_button()
+        first_script_page_two.press_yes_button()
         scooter_transition = Transitions(driver)
         scooter_transition.press_confirmation_button()
         scooter_transition.press_scooter_button()
@@ -40,8 +51,20 @@ class TestOrderAccordingFirstScript:
 
     @allure.title("Проверка перехода со страницы оформления заказа на страницу Яндекс.Дзен")
     def test_transition_from_order_page_to_dzen_page(self, driver):
-        scooter_confirmation = TestOrderAccordingFirstScript()
-        scooter_confirmation.test_order_form_with_first_script(driver)
+        name = FirstScript.name
+        last_name = FirstScript.last_name
+        address = FirstScript.address
+        phone = FirstScript.phone
+        first_script_page_one = PageOne(driver)
+        first_script_page_one.press_main_page_upper_order_button()
+        first_script_page_one.press_cookies_button()
+        first_script_page_one.fill_up_order_form_page_one(name, last_name, address, phone)
+        first_script_page_one.press_next_button()
+        first_script_page_two = PageTwo(driver)
+        comment = FirstCommentForCourier.comment_for_first_script
+        first_script_page_two.fill_up_order_form_page_two_with_first_set_of_data(comment)
+        first_script_page_two.press_order_button()
+        first_script_page_two.press_yes_button()
         dzen_transition = Transitions(driver)
         dzen_transition.press_confirmation_button()
         dzen_transition.press_dzen_button()
--- tests/test_second_script.py
+++ tests/test_second_script.py
@@ -2,8 +2,7 @@ import allure
 
 
 from pages.page_one import PageOne
-from data import SecondScript
-from data import SecondCommentForCourier
+from data import *
 from pages.page_two import PageTwo
 from pages.transitions import Transitions
 from curl import *
@@ -26,12 +25,24 @@ class TestOrderWithSecondScript:
         second_script_page_two.press_order_button()
         second_script_page_two.press_yes_button()
         order_confirmation = second_script_page_two.get_text_confirmation()
-        assert order_confirmation == "Посмотреть статус"
+        assert order_confirmation == CHECKUP_STATUS
 
     @allure.title("Проверка перехода со страницы оформления заказа на главную страницу сайта")
     def test_transition_from_order_page_to_main_page(self, driver):
-        scooter_confirmation = TestOrderWithSecondScript()
-        scooter_confirmation.test_order_form_with_second_script(driver)
+        name = SecondScript.name
+        last_name = SecondScript.last_name
+        address = SecondScript.address
+        phone = SecondScript.phone
+        second_script_page_one = PageOne(driver)
+        second_script_page_one.press_main_page_lower_order_button()
+        second_script_page_one.press_cookies_button()
+        second_script_page_one.fill_up_order_form_page_one(name, last_name, address, phone)
+        second_script_page_one.press_next_button()
+        second_script_page_two = PageTwo(driver)
+        comment = SecondCommentForCourier.comment_for_second_script
+        second_script_page_two.fill_up_order_form_page_two_with_second_set_of_data(comment)
+        second_script_page_two.press_order_button()
+        second_script_page_two.press_yes_button()
         scooter_transition = Transitions(driver)
         scooter_transition.press_confirmation_button()
         scooter_transition.press_scooter_button()
@@ -39,8 +50,20 @@ class TestOrderWithSecondScript:
 
     @allure.title("Проверка перехода со страницы оформления заказа на страницу Яндекс.Дзен")
     def test_transition_from_order_page_to_dzen_page(self, driver):
-        scooter_confirmation = TestOrderWithSecondScript()
-        scooter_confirmation.test_order_form_with_second_script(driver)
+        name = SecondScript.name
+        last_name = SecondScript.last_name
+        address = SecondScript.address
+        phone = SecondScript.phone
+        second_script_page_one = PageOne(driver)
+        second_script_page_one.press_main_page_lower_order_button()
+        second_script_page_one.press_cookies_button()
+        second_script_page_one.fill_up_order_form_page_one(name, last_name, address, phone)
+        second_script_page_one.press_next_button()
+        second_script_page_two = PageTwo(driver)
+        comment = SecondCommentForCourier.comment_for_second_script
+        second_script_page_two.fill_up_order_form_page_two_with_second_set_of_data(comment)
+        second_script_page_two.press_order_button()
+        second_script_page_two.press_yes_button()
         dzen_transition = Transitions(driver)
         dzen_transition.press_confirmation_button()
         dzen_transition.press_dzen_button()
